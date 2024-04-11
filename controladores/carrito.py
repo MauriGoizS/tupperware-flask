843ffc4ba674b30ce846cc560f58e2216806a06e
Irving Didier Sarmiento De La Cruz <130480598+IrvingDidierRCCL@users.noreply.github.com>
2024-04-11 00:37:02 -0600

    rest carrito

--- controladores/carrito.py
+++ controladores/carrito.py
@@ -4,7 +4,6 @@ from flask import request
 from app import create_app
 from mongo import mongo
 from flask import Blueprint, jsonify
-from bson.json_util import dumps
 
 carrito = Blueprint("carrito", __name__)
 app = create_app()
@@ -24,17 +23,27 @@ def obtener_carrito_por_id(id):
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
-@carrito.route('/carrito', methods=['POST'])
-def crear_carrito(id):
-    query = {'_id': ObjectId(id)}
-    project = {"_id": 0}
+@carrito.route('/carrito/nuevo', methods=['POST'])
+def crear_carrito():
+    productos   = request.json["productos"]
+    
+    if request.method == 'POST':
+        carrito = {
+            "fecha_carrito": datetime.now(),
+            "productos": productos
+        }
     try:
-        resultado = mongo.db.carrito.find_one(query, project)
-        print(resultado)
-
-        if resultado: 
-            return jsonify(resultado)
+        _id = mongo.db.carrito.insert_one(carrito)
+        if _id:
+        # Si la consulta es exitosa, devuelve los datos en formato JSON
+            return jsonify({
+                "mensaje" : "Carrito insertado con éxito",
+                "_id": str(_id.inserted_id)
+            }), 200
         else:
-            return jsonify({"mensaje": "carrito no encontrado"}), 404
+        # Si no se pudo insertar el documento, devuelve un mensaje
+            return jsonify({"error": "Hubo un problema al momento de guardar el carrito"}), 500
     except Exception as e:
+        # Manejo de la excepción, puedes personalizar el mensaje de error según tus
+        #necesidades
         return jsonify({"error": str(e)}), 500
